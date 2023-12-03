adf2fc310729b15aad4d5d2633912613f8a651bf
SarcAceTic <sarcacetic@gmail.com>
2023-12-03 19:50:06 +0700

    Added an About Me section.

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <v-container>
+    <h1 class="text-center my-1">About Me</h1>
+    <p>
+      Hi there! I'm an Indonesian teenager whose really passionate about front-end engineering and
+      programming! I'm also open to learning new things, other than programming, such as Arts, and
+      Mathematics! 
+    </p>
+  </v-container>
+</template>
+
+<style scoped></style>
