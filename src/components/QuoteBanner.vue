c53d54f2d21c650214bc885bf32333f03871c2da
SarcAceTic <sarcacetic@gmail.com>
2023-12-03 17:43:13 +0700

    Added an inspirational banner quote

---
+++ src/components/QuoteBanner.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <div class="image-container">
+    <v-img class="banner-image" src="/bannerimage.png" />
+    <p class="w-75 text-center quote-text"><i>"Your time is limited, don't waste it living someone else's life."</i><br>- Steve Jobs</p>
+  </div>
+</template>
+
+<style scoped>
+.image-container {
+  position: relative;
+  background: rgb(54, 11, 73);
+  background: linear-gradient(0deg, rgba(54, 11, 73, 1) 0%, rgba(98, 18, 172, 1) 100%);
+}
+.banner-image {
+  position: relative;
+  opacity: 0.35;
+  display: block;
+}
+.quote-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
