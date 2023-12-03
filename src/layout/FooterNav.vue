09d06d195b22adb601b2c91dacaf2cd2fb983ec6
SarcAceTic <sarcacetic@gmail.com>
2023-12-03 13:12:23 +0700

    Footer created

--- src/layout/FooterNav.vue
+++ src/layout/FooterNav.vue
@@ -1,8 +1,14 @@
-<template>
-    <v-footer>
-        Hmm
-    </v-footer>
-</template>
-
 <script setup>
+const contacts = [
+  { name: 'mdi-github', href: 'https://github.com/SarcAceTic' },
+  { name: 'mdi-twitter', href: 'https://twitter.com/SarcAce33749' },
+  { name: 'mdi-youtube', href: 'https://www.youtube.com/channel/UC3f4hB4foeVvAR-Kk2k2vCQ' }
+]
 </script>
+
+<template>
+  <v-footer class="pa-4 d-flex flex-column">
+    <div><v-btn class="mx-4" icon color="transparent" elevation="0" v-for="contact in contacts"><v-icon size="25">{{ contact.name }}</v-icon></v-btn></div>
+   <p class="text-grey">Made with Vuetify 3 and Vite 3</p>
+  </v-footer>
+</template>
