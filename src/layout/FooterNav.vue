cc21d9c09106ca8437d824540cdc568095a27506
SarcAceTic <sarcacetic@gmail.com>
2023-12-04 13:33:32 +0700

    Optimized Footer

--- src/layout/FooterNav.vue
+++ src/layout/FooterNav.vue
@@ -1,14 +1,17 @@
-<script setup>
-const contacts = [
-  { name: 'mdi-github', href: 'https://github.com/SarcAceTic' },
-  { name: 'mdi-twitter', href: 'https://twitter.com/SarcAce33749' },
-  { name: 'mdi-youtube', href: 'https://www.youtube.com/channel/UC3f4hB4foeVvAR-Kk2k2vCQ' }
-]
-</script>
+<script setup></script>
 
 <template>
-  <v-footer class="pa-4 d-flex flex-column">
-    <div><v-btn class="mx-4" icon color="transparent" elevation="0" v-for="contact in contacts"><v-icon size="25">{{ contact.name }}</v-icon></v-btn></div>
-   <p class="text-grey">Made with Vuetify 3 and Vite 3</p>
+  <v-footer class="pa-2 d-flex flex-column">
+    <p class="text-grey">
+      Hosted on
+      <b>Vercel</b>
+    </p>
+    <p class="text-grey">
+      Made with Vuetify 3, Vite 3 and
+      <span class="text-pink-lighten-1">
+        Love
+        <v-icon>mdi-heart</v-icon>
+      </span>
+    </p>
   </v-footer>
 </template>
--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,13 +1,10 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
+      <v-icon icon="mdi-circle-slice-3" />
     </v-app-bar-title>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
 </script>
