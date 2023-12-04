6e344d7fbb4da9dc4baf5f3968e47669139a6325
SarcAceTic <sarcacetic@gmail.com>
2023-12-04 09:50:20 +0700

    Edited some heading colors, and added a My Skills section

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,12 +2,14 @@
 
 <template>
   <v-container>
-    <h1 class="text-center my-1">About Me</h1>
-    <p>
-      Hi there! I'm an Indonesian teenager whose really passionate about front-end engineering and
-      programming! I'm also open to learning new things, other than programming, such as Arts, and
-      Mathematics! 
-    </p>
+    <section>
+      <h2 class="text-purple-accent-3 text-center my-1">About Me</h2>
+      <p>
+        Hi there! I'm an Indonesian teenager whose really passionate about front-end engineering and
+        programming! I'm also open to learning new things, other than programming, such as Arts, and
+        Mathematics!
+      </p>
+    </section>
   </v-container>
 </template>
 
---
+++ src/components/MProfile.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+
+<template>
+  <v-container>
+    <section>
+      <v-row class="justify-center" no-gutters>
+        <span class="my-2 grad-border">
+          <v-avatar size="85" class="main-avatar" image="/myPFP.jpg"></v-avatar>
+        </span>
+        <div class="d-flex flex-column">
+          <h1 class="text-purple-accent-3 text-center mb-1">SarcAceTic / Zhell</h1>
+          <span class="text-center mb-1">
+            A younglin learning the ropes of the world AND front-end engineering!
+          </span>
+        </div>
+      </v-row>
+    </section>
+  </v-container>
+</template>
+
+<style scoped>
+.grad-border {
+  display: inline-block;
+  width: auto;
+  height: 100%;
+  background: -webkit-linear-gradient(left top, purple 0%, rgb(76, 0, 255) 100%);
+  border-radius: 1000px;
+  padding: 3px;
+}
+</style>
