72261fe7eb4b62712f0dced222168fbe40af7e52
SarcAceTic <sarcacetic@gmail.com>
2023-12-04 13:32:49 +0700

    Added all the sections onto the main View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,22 @@
 <script setup>
+import QuoteBannerVue from '../components/QuoteBanner.vue'
+import MainProfileVue from '../components/MProfile.vue'
+import AboutMe from '../components/AboutMe.vue'
+import MySkills from '../components/MySkills.vue'
+import MyContacts from '../components/MyContacts.vue'
 </script>
 
 <template>
-  
+  <QuoteBannerVue />
+  <MainProfileVue />
+  <v-divider class="mx-4"></v-divider>
+  <AboutMe />
+  <v-divider class="mx-4"></v-divider>
+  <MySkills /> 
+  <v-divider class="mx-4"></v-divider>
+  <MyContacts />
 </template>
+
+<style scoped>
+
+</style>
